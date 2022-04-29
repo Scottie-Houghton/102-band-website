66d75d42f2050647c95c911b4056a939f3855f3f
Scottie-Houghton <scott.c.houghton@gmail.com>
2022-04-29 01:46:12 -0700

    Wrapped the JavaScript that I added in the previous lab in functions as directed in Lab: 07

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 'use strict';
 
-let userGenre = prompt ("What is your favorite genre of music?");
-
-console.log(userGenre);
-
-let band = prompt("Ah, a " + userGenre + " lover! What is your favorite band?");
-
-console.log(band);
-
-let scottiesBand = prompt("Dude! " + band + " rock!! Can you guess my favorite band?");
-
-scottiesBand.toLowerCase();
-
-console.log(scottiesBand);
-
-if (scottiesBand.toLowerCase() == "queen") {
-    document.write("<p id='scotties-band'>" + "Whoa, good guess! Impressive!!" + "</p>");
-} else if (scottiesBand.toLowerCase() == "talking heads") {
-    document.write("<p id='scotties-band'>" + "Oh, so close! They're actually my 2nd favorite band!" + "</p>");
-} else {
-    document.write("<p id='scotties-band'>" + "Nope, but they rock, too!" + "</p>");
+function taste () {
+    let userGenre = prompt ("What is your favorite genre of music?");
+    let band = prompt("Ah, a " + userGenre + " lover! What is your favorite band?");
+    let guessBand = prompt("Dude! " + band + " rock!! Can you guess my favorite band?");
+    // if (guessBand.toLowerCase() != "queen") {
+        // prompt("Nope, but they rock, too! Try again.")
+    // }
+    return guessBand
 }
+
+let scottiesBand = taste ()
+
+function firstTry (scottiesBand) {
+    if (scottiesBand.toLowerCase() == "queen") {
+        document.write("<p id='scotties-band'>" + "Whoa, good guess! Impressive!!" + "</p>");
+    } else if (scottiesBand.toLowerCase() == "talking heads") {
+        document.write("<p id='scotties-band'>" + "Oh, so close! They're actually my 2nd favorite band! Give it another shot." + "</p>");
+    } else {
+        document.write("<p id='scotties-band'>" + "Nope, but they rock, too!" + "</p>");
+        // document.write("<p id='scotties-band'>" + "Sorry, wrong again, but another great band!" + "</p>");
+    }
+    return scottiesBand
+}
+
+firstTry (scottiesBand)
